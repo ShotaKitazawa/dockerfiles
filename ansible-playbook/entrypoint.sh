1843e2dd8beed26bab6f3a896fbffb846acf53e1
ShotaKitazawa <skitazawa1121@gmail.com>
2020-09-24 22:40:39 +0900

    ansible-playbook: --check有効化

--- ansible-playbook/entrypoint.sh
+++ ansible-playbook/entrypoint.sh
@@ -2,12 +2,18 @@
 
 OPT=""
 
+if [ "$CHECK" == "true" ]; then
+  OPT="$OPT --check"
+fi
+
 if [ "$INVENTORY_FILE" != "" ]; then
   OPT="$OPT -i $INVENTORY_FILE"
 fi
+
 if [ "$TARGET_USER" != "" ]; then
   OPT="$OPT -u $TARGET_USER"
 fi
+
 if [ "$PRIVATE_KEY" != "" ] || [ "$PRIVATE_KEY_FILE" != "" ]; then
   if [ "$PRIVATE_KEY" != "" ]; then
     echo "$PRIVATE_KEY" > /tmp/private_key
@@ -17,6 +23,7 @@ if [ "$PRIVATE_KEY" != "" ] || [ "$PRIVATE_KEY_FILE" != "" ]; then
     OPT="$OPT --private-key $PRIVATE_KEY_FILE"
   fi
 fi
+
 if [ "$BECOME_PASS" != "" ] || [ "$SSH_PASS" != "" ]; then
   if [ "$BECOME_PASS" != "" ]; then
     echo "ansible_become_pass: $BECOME_PASS" >> /tmp/password.yml
@@ -26,10 +33,12 @@ if [ "$BECOME_PASS" != "" ] || [ "$SSH_PASS" != "" ]; then
   fi
   OPT="$OPT --extra-vars=@/tmp/password.yml"
 fi
+
 if [ "$VAULT_PASS" != "" ]; then
   echo "$VAULT_PASS" > /tmp/vault_pass.txt
   OPT="$OPT --vault-password-file /tmp/vault_pass.txt"
 fi
+
 
 set -ux
 ansible-playbook $OPT $PLAYBOOK_FILE
